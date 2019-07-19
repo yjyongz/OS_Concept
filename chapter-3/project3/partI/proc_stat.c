a1af59c1562202e9dfcdbd1f365dfe974d30cfac
Shengjun Ma <firegun17@hotmail.com>
2019-07-19 09:42:25 -0700

    project 3

--- chapter-3/project3/partI/proc_stat.c
+++ chapter-3/project3/partI/proc_stat.c
@@ -7,17 +7,31 @@
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
+#include <linux/jiffies.h>
+#include <linux/cred.h>
 
+int pcpu(struct task_struct *task) {
+    int percentage = 0;
+    //int total_time = task->utime + task->stime;
+    //total_time += 
+    //task->real_start_time -  
+    return percentage;
+}
 int proc_init(void) {
     struct task_struct *task;
-    unsigned long long used_jiffies= 0;
+    //unsigned long long used_jiffies= 0;
+    char comms[16] = {0};
 
     printk(KERN_INFO "F\tS\tUID\tPID\tPPID\tC\tPRI\tNI\tADDR\tSZ\tWCHAN\tTTY\tTIME\tCMD\n");
+    
     for_each_process(task) {
-        printk(KERN_INFO "%d\t%d\t%d\t%d\t\%d\t", 
-               task->flags, task->state, task->cred->uid, 
-               task->pid, task->parent->pid);
-        used_jiffies += task->
+        memset(comms, 0, 16);
+        get_task_comm(comms, task);
+        printk(KERN_INFO "%d\t%ld\t%u\t%d\t%d\t"
+               "%d\t%d\t%d\t%s\t%lld\t%lld\n", 
+               task->flags, task->state, task_uid(task).val, task->pid, task->parent->pid, 
+               pcpu(task), task->rt_priority, task_nice(task), comms, 
+               task->start_time, task->real_start_time);
     }
     return 0;
 }
